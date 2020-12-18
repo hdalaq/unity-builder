90243da07ab9fccb39c36108d134478501053d71
Husam Aldalaq <hdalaq@gmail.com>
2020-12-18 10:17:34 -0500

    one more time!

--- action/steps/activate.sh
+++ action/steps/activate.sh
@@ -61,12 +61,14 @@ elif [[ -n "$UNITY_SERIAL" && -n "$UNITY_EMAIL" && -n "$UNITY_PASSWORD" ]]; then
   echo "Requesting activation (professional license)"
   
   # Set the license file path
-  FILE_PATH=invalid-project-path
+  FILE_PATH=../temp-project-path
+  
+  mkdir -p $FILE_PATH
+  cd $FILE_PATH
 
   # Activate license
   unity-editor \
     -nographics \
-    -projectPath $FILE_PATH \
     -logFile /dev/stdout \
     -quit \
     -serial "$UNITY_SERIAL" \
