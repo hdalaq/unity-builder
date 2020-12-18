416c9f7d3b3a005f49f2fd963716a2a37fac5d64
Husam Aldalaq <hdalaq@gmail.com>
2020-12-18 11:50:56 -0500

    does the "?" work!

--- action/steps/activate.sh
+++ action/steps/activate.sh
@@ -66,14 +66,13 @@ elif [[ -n "$UNITY_SERIAL" && -n "$UNITY_EMAIL" && -n "$UNITY_PASSWORD" ]]; then
 #   mkdir -p $FILE_PATH
 #   cd $FILE_PATH
   
-  UNITY_PROJECT_PATH="$GITHUB_WORKSPACE/$PROJECT_PATH"
-  echo "Using project path \"$UNITY_PROJECT_PATH\"."
+#   UNITY_PROJECT_PATH="$GITHUB_WORKSPACE/$PROJECT_PATH"
+#   echo "Using project path \"$UNITY_PROJECT_PATH\"."
 
   # Activate license
   unity-editor \
     -nographics \
-    -projectPath "$UNITY_PROJECT_PATH" \
-    -buildTarget "$BUILD_TARGET" \
+    -projectPath "?" \
     -logFile /dev/stdout \
     -quit \
     -serial "$UNITY_SERIAL" \
